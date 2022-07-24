d482c18faa19cdfde608f36f26145b30357c364f
nisrine <34342007+nisrinekane@users.noreply.github.com>
2022-07-23 23:43:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("page loaded...");
+
+
+// need to click on video to trigger functions
+
+// play video on mouse over
+function playVid(element) {
+    element.play();
+}
+
+
+// pause video on mouse out
+function pauseVid(element) {
+    element.pause();
+}
